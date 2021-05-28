8483d951d2f5c97551ef5f54345ca4e015ebe8ac
zsda111 <>
2021-05-28 17:46:52 +0000

    tried to animate the blue box.

--- motion.js
+++ motion.js
@@ -9,6 +9,21 @@ function myMove() {
 	  elem.style.top = posTop + "px"; 
 	  elem.style.left = posLeft + "px"; 
 	  if ((posTop == 350) && (posLeft == 350))
-		  clearInterval(id);
-  }
-}
+		  clearInterval(id)
+      }
+    }
+function blue() {
+ var elem = document.getElementById("animate2");
+ var posTop = 0;
+ var posLeft = 0;
+ var id= setInterval(frame, 5);
+  function frame(){
+   posTop++;
+   posleft++;
+   elem.style.top = postTop + "px";
+   elem.style.left = posLeft + "px";
+    if ((posTop==350) && (posLeft ==350))
+    clearInterval(id)
+   } 
+ }
+		  
