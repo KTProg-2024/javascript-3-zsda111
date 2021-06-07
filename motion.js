17b422c249e02cc161727f05b661099cd5b8fd94
zsda111 <>
2021-06-07 15:42:36 +0000

    finished project

--- motion.js
+++ motion.js
@@ -15,14 +15,14 @@ function myMove() {
 function blue() {
  var elem = document.getElementById("animate2");
  var posTop = 0;
- var posLeft = 0;
+ var posLeft = 350;
  var id= setInterval(frame, 5);
   function frame(){
    posTop++;
-   posleft++;
-   elem.style.top = postTop + "px";
+   posLeft--;
+   elem.style.top = posTop + "px";
    elem.style.left = posLeft + "px";
-    if ((posTop==350) && (posLeft ==350))
+    if ((posTop == 350) && (posLeft == 0))
     clearInterval(id)
    } 
  }
